399235ac00ddeb464ea15d8306bd39921967c26e
Eugene Kuntsevich <eugene.kuntsevich@specific-group.com>
2021-08-14 07:53:53 +0300

    autoservice renamed to carservice

--- react-project-steps/step4/autoservice-frontend_SNAPSHOT-step4/src/app/pages/welcome/welcom-page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './welcome-page.scss';
-
-export const WelcomePage: React.FC<any> = () => {
-  return(
-      <div>Welcome to autoservice</div>
-  )
-}
-
---
+++ react-project-steps/step4/carservice-frontend_SNAPSHOT-step4/src/app/pages/welcome/welcom-page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './welcome-page.scss';
+
+export const WelcomePage: React.FC<any> = () => {
+  return(
+      <div>Welcome to carservice</div>
+  )
+}
+
